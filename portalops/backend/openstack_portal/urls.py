9b7d4a1f3e06adce1c0e56123902d924fd8228f3
tuanthanh12x <tuanthanh.greencloud@gmail.com>
2025-06-03 09:14:17 +0700

    ksdk

--- portalops/backend/openstack_portal/urls.py
+++ portalops/backend/openstack_portal/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('instance-option/', InstanceOptionsView.as_view(), name='instance-option'),
     path('volume-option/', VolumeOptionsView.as_view(), name='volume-option'),
     path('compute/instances/', CreateInstanceAPI.as_view(), name='create-instance'),
-    path('volumes', VolumeAPI.as_view(), name='volume_api'),
+    path('storage/volumes', VolumeAPI.as_view(), name='volume_api'),
     path('compute/images/', CreateImageAPI.as_view(), name='create-image'),
     path('compute/keypair/', KeypairView.as_view(), name='keypair'),
     path("compute/instances/<str:id>/action/", InstanceActionAPI.as_view()),
