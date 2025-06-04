a6f564fee3b82035f0434c6abf6d31349db8280b
tuanthanh12x <tuanthanh.greencloud@gmail.com>
2025-06-04 15:24:57 +0700

    ks

--- portalops/backend/userauth/urls.py
+++ portalops/backend/userauth/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import LoginWithProjectView, GetProjectsView
+from .views import LoginWithProjectView
 
 urlpatterns = [
     path('login/', LoginWithProjectView.as_view(), name='login'),
