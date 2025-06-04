25ecadfd0bca96b2c7f813c3b88925f17b758b1e
tuanthanh12x <tuanthanh.greencloud@gmail.com>
2025-06-04 15:25:45 +0700

    ks

--- portalops/backend/userauth/urls.py
+++ portalops/backend/userauth/urls.py
@@ -3,5 +3,4 @@ from .views import LoginWithProjectView
 
 urlpatterns = [
     path('login/', LoginWithProjectView.as_view(), name='login'),
-    path("projects/", GetProjectsView.as_view(), name="get-projects"),
 ]
