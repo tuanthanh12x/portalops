973402b306a829ba0cae6d3032345d72b76b342c
tuanthanh12x <tuanthanh.greencloud@gmail.com>
2025-06-09 15:08:15 +0700

    lskd

---
+++ portalops/frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",  // all React components and pages
+    "./public/index.html"           // your main HTML file
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
