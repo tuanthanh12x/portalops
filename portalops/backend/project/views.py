5b6adffe61a3dc97999b0d5512d336c274cffc4e
tuanthanh12x <tuanthanh.greencloud@gmail.com>
2025-07-10 16:37:27 +0700

    ksdk

--- portalops/backend/project/views.py
+++ portalops/backend/project/views.py
@@ -451,29 +451,30 @@ class AdminProjectDetailView(APIView):
         compute_url = settings.OPENSTACK_COMPUTE_URL
         block_storage_url = settings.OPENSTACK_BLOCK_STORAGE_URL
         try:
+            atoken = get_admin_token()
             # --- Compute limits ---
             nova_response = requests.get(
                 f"{compute_url}/limits",
-                headers={"X-Auth-Token": token}
+                headers={"X-Auth-Token": atoken}
             )
             nova_response.raise_for_status()
             absolute = nova_response.json().get("limits", {}).get("absolute", {})
 
-
+            cpu_used = absolute.get("totalCoresUsed", 0)
+            cpu_limit = absolute.get("maxTotalCores", 0)
+            ram_used = absolute.get("totalRAMUsed", 0)
+            ram_limit = absolute.get("maxTotalRAMSize", 0)
 
             # --- Block storage limits ---
             cinder_url = f"{block_storage_url}/{project_id}/os-quota-sets/{openstack_id}?usage=True"
-            atoken = get_admin_token()
+
             cinder_response = requests.get(
                 cinder_url,
                 headers={"X-Auth-Token": atoken}
             )
             cinder_response.raise_for_status()
             quota = cinder_response.json().get("quota_set", {})
-            cpu_used = quota.get("gigabytes", {}).get("in_use", 0)
-            cpu_limit = quota.get("gigabytes", {}).get("limit", 0)
-            ram_used =quota.get("gigabytes", {}).get("in_use", 0)
-            ram_limit = quota.get("gigabytes", {}).get("limit", 0)
+
             storage_used = quota.get("gigabytes", {}).get("in_use", 0)
             storage_limit = quota.get("gigabytes", {}).get("limit", 0)
 
